04ebc145fa468f827d2ec72437c037cadb0c0909
Smruti-24 <smrutiranjanpadhy123@gmail.com>
2025-07-27 22:09:39 +0530

    Initial Commit

--- s3.tf
+++ s3.tf
@@ -1,4 +1,10 @@
 resource "aws_s3_bucket" "codepipeline_artifacts" {
+<<<<<<< HEAD
   bucket = "project-artifacts-puru"
   acl    = "private"
 } 
+=======
+  bucket = "project-artifacts-8457"
+  acl    = "private"
+} 
+>>>>>>> 2ff74ac (Initial Commit)
